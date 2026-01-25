2c108dd60417e7e7711ccb0d19cdda9536c1b0a1
Umer Mahmood <umermahmooduaf@gmail.com>
2026-01-25 14:53:28 +0100

    Update day1_basics.py

---
+++ week1_basics/day1_basics.py
@@ -0,0 +1,12 @@
+# Variables
+a = 10
+b = 5
+print("sum", a + b)
+
+# lists 
+samples = ("S1", "S2", "S3")
+print("Number of samples:", len(samples))
+print("First sample:", samples[0])
+
+for s in samples:
+    print("Processing:", s)
